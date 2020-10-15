f345ea373666c64cfda3d0c8c440b178aa5b53ac
blangwell <b.langwell@outlook.com>
2020-10-15 12:32:49 -0700

    [lawnmower] Add css border to canvas elem, dynamically change canvas resolution in app.js

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const game = document.getElementById('main-game');
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log(game);
+  game.style.width = '100%';
+  game.style.height = 'auto';
+})
