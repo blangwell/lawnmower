815fc55a4417b8ac54de63e560476fbb7e52f737
blangwell <b.langwell@outlook.com>
2020-10-19 11:06:10 -0700

    [lawnmower] Set up Phaser project and bundle modules

---
+++ game.js
@@ -0,0 +1,4 @@
+import Phaser from 'phaser'
+
+new Phaser.Game()
+
--- js/app.js
+++
@@ -1,7 +0,0 @@
-const game = document.getElementById('main-game');
-
-document.addEventListener('DOMContentLoaded', () => {
-  console.log(game);
-  game.style.width = '100%';
-  game.style.height = 'auto';
-})
